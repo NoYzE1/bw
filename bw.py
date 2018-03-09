9a4332d8e3c0a16142526490a7968b1a71f0094b
NoYzE1 <NoYzE@hotmail.de>
2018-03-09 18:31:16 +0100

    Update bw.py

--- bw.py
+++ bw.py
@@ -4,7 +4,7 @@ import pyaudio
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32, channels=2, rate=44100, output=1)
 factor_l = 70 * (math.pi * 2) / 44100 # Frequency Left
-factor_r = 90 * (math.pi * 2) / 44100 # Frequency right
+factor_r = 90 * (math.pi * 2) / 44100 # Frequency Right
 chunk_l = numpy.sin(numpy.arange(44100) * factor_l)
 chunk_r = numpy.sin(numpy.arange(44100) * factor_r)
 chunk = []
